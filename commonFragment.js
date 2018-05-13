6f24bd0410d2a79e1d8cfe937db4bcac09ccea4e
dongsi233 <13502268327@163.com>
2018-05-13 23:17:59 +0800

    array remove

---
+++ commonFragment.js
@@ -0,0 +1,6 @@
+//数组删除指定项
+Array.prototype.remove = function (from, to) {
+	var rest = this.slice((to || from) + 1 || this.length);
+	this.length = from < 0 ? this.length + from : from;
+	return this.push.apply(this, rest);
+}
